7903d8dbbd4380fd5ddddd90dd723b8612eceda5
Cade Mathurin <cade.mathurin245@gmail.com>
2026-07-01 22:19:23 +0100

    Add initial project structure with navbar and styling
    - Created logo512.png for application branding.
    - Added manifest.json for PWA support with app icons and display settings.
    - Included robots.txt to manage web crawler access.
    - Developed App.css for application styling, including responsive navbar.
    - Implemented App.js with a functional Navbar component and logo.
    - Added index.css for global styles and body background.
    - Set up index.js as the entry point for the React application.
    - Created logo.svg for the application logo.

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+
+
+function Navbar() {
+const [isOpen, setIsOpen] = React.useState(false);
+  
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <NavLogo />
+        <NavMenuButton isOpen={isOpen} setIsOpen={setIsOpen} />
+        <NavMenu isOpen={isOpen} />
+      </div>
+    </nav>
+  )
+}
+
+function NavLogo() {
+  return (
+    <div className="navbar-logo">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+  );
+}
+
+function NavMenuButton({ isOpen, setIsOpen }) {
+  return (
+    <button className={isOpen ? "navbar-toggle open" : "navbar-toggle"} aria-label="Toggle navigation" onClick={() => setIsOpen(!isOpen)}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </button>
+  );
+}
+
+function NavMenu({ isOpen }) {
+  return (
+    <ul className={ isOpen ? "navbar-menu open" : "navbar-menu"}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">CV</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Skills</a></li>
+          <li><a href="#">Projects</a></li>
+        </ul>
+  );
+}
+
+export default App;
